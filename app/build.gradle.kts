fae1bfa4114e02643e90c7126153b5a693e4e700
ErrorTek <jakub.baranski.33@gmail.com>
2024-12-24 10:00:06 -0800

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.errortek.jpb24"
         minSdk = 27
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
